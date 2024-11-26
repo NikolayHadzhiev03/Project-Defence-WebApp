08d43b03f7ac6eab8af3ec7659321c557fda07a1
NikolayHadzhiev03 <nikolaiHadzhiev03@gmail.com>
2024-11-26 12:30:30 +0200

    Added some basic pages (HTML/CSS) and set up basic routing to navigate between them.

--- Front-End/ForumApplication/src/app/app.routes.ts
+++ Front-End/ForumApplication/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "home",component : HomeComponent},
+    {path : "login",component : LoginComponent},
+    {path : "register",component : RegisterComponent},
+    {path : "profile",component : ProfileComponent},
+    {path : "**",component : ErrorPageComponent}
+    
+];
