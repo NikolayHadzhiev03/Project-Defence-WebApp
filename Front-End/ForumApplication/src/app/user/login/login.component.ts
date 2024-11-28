265297d9c8d1f1648d76766ff30af6974b562757
NikolayHadzhiev03 <nikolaiHadzhiev03@gmail.com>
2024-11-28 16:39:03 +0200

    Email validation
    Implemented a custom email validator using directives for login and register.

--- Front-End/ForumApplication/src/app/user/login/login.component.ts
+++ Front-End/ForumApplication/src/app/user/login/login.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { EmailDirective } from '../../directives/email.directive';
+import {  DOMAINS } from '../../constants';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule,EmailDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  domain = DOMAINS;
+
+  login(form : NgForm){
+    if(form.invalid){
+      return console.error("Invalid form");
+      
+    }
+
+    console.log(form);
+    
+    
+
+  }
 
 }
--- Front-End/ForumApplication/src/app/user/register/register.component.ts
+++ Front-End/ForumApplication/src/app/user/register/register.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { from } from 'rxjs';
+import { EmailDirective } from '../../directives/email.directive';
+import { DOMAINS } from '../../constants';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule,EmailDirective],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  domain = DOMAINS;
+  register(form : NgForm){
+    if(form.invalid){
+      console.error('Form is invalid')
+      
+    }
+  }
 
 }
